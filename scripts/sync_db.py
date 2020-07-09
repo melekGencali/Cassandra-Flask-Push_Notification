a3f3a1a526d8b3ecbff618f1028bf1ec4ca2f705
melekGencali <68080787+melekGencali@users.noreply.github.com>
2020-07-10 00:55:07 +0300

    Add files via upload

--- scripts/sync_db.py
+++ scripts/sync_db.py
@@ -1,1 +1,18 @@
+from cassandra.auth import PlainTextAuthProvider
+from cassandra.cqlengine import connection
+from cassandra.cqlengine.management import sync_table
+
+
+from models.user import Person
+
+
+__author__="melek"
+
+auth_provider = PlainTextAuthProvider(
+        username='cassandra', password='cassandra')
+
+connection.setup(['localhost'],
+                 protocol_version=4, retry_connect=True, auth_provider=auth_provider)
+
+sync_table(Person)
 
